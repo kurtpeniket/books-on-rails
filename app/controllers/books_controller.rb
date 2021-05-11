b395101661fba2eff08ebb8df377da47dc7e8d31
Kurt Peniket <kurt.peniket@gmail.com>
2021-05-11 14:16:41 +0100

    new migrations, controllers and styles

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,32 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user = current_user
+
+    if @book.save!
+      flash[:alert] = 'Added!'
+      redirect_to book_path(@book)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :rating)
+  end
+end
---
+++ app/models/book.rb
@@ -0,0 +1,7 @@
+class Book < ApplicationRecord
+  belongs_to :user
+  # validates :title, presence: true,
+  # validates :author, presence: true,
+  # validates :rating, presence: true, numericality: { only_integer: true }
+
+end
